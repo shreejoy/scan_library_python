1c18ed906d873549c56c94297af4334407ffc21a
shreejoy <pshreejoy15@gmail.com>
2022-02-04 09:54:00 +0530

    Update script

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 import sys
 import json
 
-def scan(fn, depth = 1):
-    if depth > 2:
+success, fail = [], []
+def scan(fn, depth):
+    if depth == 0:
         return {}
     func = eval(fn)
     result = dict()
@@ -10,19 +11,21 @@ def scan(fn, depth = 1):
     result[fn]['func'] = dict()
     result[fn]['callable'] = callable(func)
     for _func in dir(func):
-        if '_' in _func or not _func.islower():
+        if '_' in _func:
             continue
-        print(f"Checking nested functions for {fn}.{_func}", end =" ")
+        # print(f"Checking nested functions for {fn}.{_func}", end =" ")
         try:
-            result[fn]['func'].update(scan(f"{fn}.{_func}", depth + 1))
-            print("...SUCCESS")
-        except:
-            print("...FAILED")
+            result[fn]['func'].update(scan(f"{fn}.{_func}", depth - 1))
+            success.append(f"{fn}.{_func}")
+        except Exception as e:
+            # sys.exit(e)
+            fail.append(f"{fn}.{_func}")
             
     return result
 
 def main():
     func = input("Enter the function name to scan > ")
+    depth = int(input("Enter the depth of scan > ") or 3)
     
     try:
         eval(func)
@@ -32,11 +35,18 @@ def main():
         except ModuleNotFoundError:
             sys.exit("Invalid function")
 
-    dump = scan(func)       
+    dump = scan(func, depth)
     dump = json.dumps(dump, indent = 4, sort_keys = False)
     file = open(f"{func}.json", "w")
     file.write(dump)
     file.close()
+    print(f"Completed scan for {func}:\n\tSuccess: {len(success)}\n\tFail: {len(fail)}")
+    print(f"Dump written to {func}.json")
+
+    if len(fail) > 0:
+        print("Failed for the following functions:", fail)
+
+    print("Done")
 
 if __name__ == "__main__":
     main()
